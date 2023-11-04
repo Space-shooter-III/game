75c90a5a97d57e6a70cc082e7818d51809fd57ac
aixoio <79969530+aixoio@users.noreply.github.com>
2023-11-05 08:55:23 +1100

    Add basic key listeners

--- game/src/main/java/com/github/spaceshooteriii/game/event/listeners/KeyBoardEventListener.java
+++ game/src/main/java/com/github/spaceshooteriii/game/event/listeners/KeyBoardEventListener.java
@@ -1,21 +1,23 @@
 package com.github.spaceshooteriii.game.event.listeners;
 
+import com.github.spaceshooteriii.game.Game;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyBoardEventListener implements KeyListener {
     @Override
     public void keyTyped(KeyEvent e) {
-
+        Game.getState().getGameStateModeManager().keyTyped(e);
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-
+        Game.getState().getGameStateModeManager().keyPressed(e);
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
+        Game.getState().getGameStateModeManager().keyReleased(e);
     }
 }
