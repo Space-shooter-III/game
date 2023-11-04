19ea87bd824fa7d742ad09f88a7f613499247d43
aixoio <79969530+aixoio@users.noreply.github.com>
2023-11-04 15:30:40 +1100

    Add sky background to home screen

--- game/src/main/java/com/github/spaceshooteriii/game/state/data/GameStateHomeManager.java
+++ game/src/main/java/com/github/spaceshooteriii/game/state/data/GameStateHomeManager.java
@@ -1,13 +1,31 @@
 package com.github.spaceshooteriii.game.state.data;
 
+import com.github.spaceshooteriii.game.Game;
 import com.github.spaceshooteriii.game.state.GameStateModeManager;
 
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 public class GameStateHomeManager extends GameStateModeManager {
 
+    private BufferedImage skyImage;
+
     @Override
     public void draw(Graphics2D g2d) {
+
+        int x = 0;
+        int y = 0;
+        final int size = 64;
+
+        for (int i = 0; i < Math.ceil((double) Game.HEIGHT / size) + 1; i++) {
+            for (int j = 0; j < Math.ceil((double) Game.HEIGHT / size) + 1; j++) {
+                g2d.drawImage(this.skyImage, x, y, size, size, null);
+                x += size;
+            }
+            y += size;
+            x = 0;
+        }
+
     }
 
     @Override
@@ -27,7 +45,7 @@ public class GameStateHomeManager extends GameStateModeManager {
 
     @Override
     public void init() {
-
+        this.skyImage = Game.TEXTRA_ALICE.getImageFrom(0, 0, 16, 16);
 
     }
 }
