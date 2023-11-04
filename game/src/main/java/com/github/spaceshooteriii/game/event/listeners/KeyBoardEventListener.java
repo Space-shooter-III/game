26e4889bd867e18ac28be9ec7677b975f875eb86
aixoio <79969530+aixoio@users.noreply.github.com>
2023-11-05 08:43:32 +1100

    Add empty key board event listener

---
+++ game/src/main/java/com/github/spaceshooteriii/game/event/listeners/KeyBoardEventListener.java
@@ -0,0 +1,21 @@
+package com.github.spaceshooteriii.game.event.listeners;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyBoardEventListener implements KeyListener {
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
