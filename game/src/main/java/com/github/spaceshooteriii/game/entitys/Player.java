4bbc5509c132ca2bb46749f29b77d89c4de574ed
aixoio <79969530+aixoio@users.noreply.github.com>
2023-11-05 11:54:55 +1100

    Load player image

--- game/src/main/java/com/github/spaceshooteriii/game/entitys/Player.java
+++ game/src/main/java/com/github/spaceshooteriii/game/entitys/Player.java
@@ -1,15 +1,17 @@
 package com.github.spaceshooteriii.game.entitys;
 
+import com.github.spaceshooteriii.game.Game;
+
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 
-public class Player extends Entity {
+public class Player extends ImageEntity {
 
     private int deg = 0;
 
     public Player(float x, float y, int width, int height) {
-        super(x, y, 0f, 0f, width, height, EntityType.PLAYER);
+        super(x, y, 0f, 0f, width, height, EntityType.PLAYER, Game.TEXTRA_ALICE.getImageFrom(0, 16, 16, 16));
     }
 
 
@@ -20,12 +22,9 @@ public class Player extends Entity {
 
         g2d.setColor(Color.BLUE);
         g2d.rotate(Math.toRadians(this.deg), (int) this.x + this.width / 2, (int) this.y + this.height / 2);
-        g2d.fillRect((int) this.x, (int) this.y, this.width, this.height);
+        super.draw(g2d);
 
         g2d.setTransform(oldXForm);
-
-        g2d.setColor(new Color(255, 0, 0, 128));
-        g2d.fillRect((int) this.x, (int) this.y, this.width, this.height);
 
     }
 
